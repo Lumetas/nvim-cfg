9c021190ceda112c33633f1bd89a91c1f2eefcf1
Lumetas <pokh9864@gmail.com>
2025-03-10 00:17:18 +0500

    added russian language

--- init.lua
+++ init.lua
@@ -7,3 +7,7 @@ require('statusline') -- Полоса
 require('lsp') -- LSP Автодополнение и прочее
 require('laravel') -- laravel
 require('commands') -- Произвольные команды
+
+
+vim.g.reset_layout_on_leave = true -- Сбрасывать ли расскладку на английский при выходе из insert mode
+require('map') -- Русский язык
