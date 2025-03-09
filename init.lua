3c70850769b8284aa1812d575475278f220e4e2b
Lumetas <pokh9864@gmail.com>
2025-03-10 00:21:26 +0500

    update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Переменные и флаги для модулей и плагинов
+vim.g.reset_layout_on_leave = true -- Сбрасывать ли расскладку на английский при выходе из insert mode
+
+
+
+
 -- Подключение модулей
 require('settings')    -- Основные настройки
 require('keymaps')     -- Ключевые отображения
@@ -7,7 +13,4 @@ require('statusline') -- Полоса
 require('lsp') -- LSP Автодополнение и прочее
 require('laravel') -- laravel
 require('commands') -- Произвольные команды
-
-
-vim.g.reset_layout_on_leave = true -- Сбрасывать ли расскладку на английский при выходе из insert mode
 require('map') -- Русский язык
