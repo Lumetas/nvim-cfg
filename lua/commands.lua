8972b701905b6e3bf70107f26e5751b65bcefd3c
Lumetas <pokh9864@gmail.com>
2025-03-12 12:47:51 +0500

    Автообновление NERDTree при смене директории

--- lua/commands.lua
+++ lua/commands.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_user_command('Format', function()
     -- Устанавливаем метку 'b' на текущей строке
     vim.cmd('normal mbgg=G`b')
 end, {})
+
+-- Автоматическое обновление древа NERDTree при смене директории
+vim.api.nvim_create_augroup('NERDTreeAutoUpdate', { clear = true })
+vim.api.nvim_create_autocmd('DirChanged', {
+  group = 'NERDTreeAutoUpdate',
+  pattern = '*',
+  callback = function()
+    vim.cmd('NERDTreeCWD')
+  end,
+})
+
