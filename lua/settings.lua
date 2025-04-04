010b87bb0d37b440762c7014e2f482c930b02220
Lumetas <pokh9864@gmail.com>
2025-04-04 16:33:57 +0500

    fileFormatUnix

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,4 +11,5 @@ vim.opt.swapfile = false
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.encoding = 'UTF-8'
 vim.opt.completeopt:remove('preview')
-vim.opt.shada = ""  
+vim.opt.shada = ""
+vim.bo.fileformat = "unix"
