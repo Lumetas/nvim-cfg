ef82114299420ff5ff0ae76ba20bea1402c94eba
lumetas <lumetas506@gmail.com>
2025-08-25 12:22:17 +0500

    debug hotkeys

--- lua/dapconf.lua
+++ lua/dapconf.lua
@@ -15,7 +15,9 @@ dap.configurations.php = {
 }
 
 vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end, { desc = '[D]ebug [C]onnect menu' })
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'Debug Next' })
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'Debug Step Over' })
+vim.keymap.set('n', '<F8>', function() require('dap').step_into() end, { desc = 'Debug Step Into' })
+vim.keymap.set('n', '<F9>', function() require('dap').step_out() end, { desc = 'Debug Step Out' })
 -- vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 -- vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end, { desc = '[D]ebug [B]reakpoint' })
