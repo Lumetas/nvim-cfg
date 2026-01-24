580c9c3a9e4340cc37502df6f3932a8c86a266ee
lumetas <lumetas506@gmail.com>
2026-01-24 18:05:30 +0500

    ht грузится по команде а не хоткеям

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,31 +1,32 @@
 return function(lnpm)
   lnpm.load("lumetas/ht.nvim", function(ht)
     ht.setup({
-		response = {
-			auto_focus_response = false 
-		}
+      response = {
+        auto_focus_response = false 
+      }
     })
-	vim.keymap.set({"n","v"},"<leader>RR", ":HT run<CR>",{desc="[R]esty [R]un request under the cursor"})
-	vim.keymap.set({"n","v"},"<leader>Rr", ":HT last<CR>",{desc="[R]esty [r]un last request"})
-	vim.keymap.set({"n","v"},"<leader>Rf", ":HT favorite<CR>",{desc="[R]esty [V]iew favorites"})   
-
+    
   end, {
   name = "ht",
   lrule = function(next)
-	  vim.keymap.set("n", "<leader>Rr", function()
-		  next()
-		  vim.cmd("HT last")
-	  end, {desc="[R]esty [r]un last request"})
+    -- Переопределяем команду HT с ленивой загрузкой
+    vim.api.nvim_create_user_command("HT", function(opts)
+      next()  -- Загружаем плагин
+      -- Запускаем переданную подкоманду
+      vim.cmd("HT " .. (opts.args or ""))
+    end, {
+      desc = "HTTP client (lazy loaded)",
+      nargs = "*",  -- Принимает аргументы (run, last, favorite и т.д.)
+      complete = function(arg_lead)
+        -- Автодополнение подкоманд после загрузки плагина
+        return {"run", "last", "favorite"}
+      end
+    })
 
-	  vim.keymap.set("n", "<leader>RF", function()
-		  next()
-		  vim.cmd("HT favorite")
-	  end, {desc="[R]esty [V]iew favorites"})
-
-	  vim.keymap.set("n", "<leader>RR", function()
-		  next()
-		  vim.cmd("HT run")
-	  end, {desc="[R]esty [R]un request under the cursor"})
+    vim.keymap.set({"n","v"},"<leader>RR", ":HT run<CR>",{desc="[R]esty [R]un request under the cursor"})
+    vim.keymap.set({"n","v"},"<leader>Rr", ":HT last<CR>",{desc="[R]esty [r]un last request"})
+    vim.keymap.set({"n","v"},"<leader>RF", ":HT favorite<CR>",{desc="[R]esty [V]iew favorites"})
+    
   end
   })
 end
