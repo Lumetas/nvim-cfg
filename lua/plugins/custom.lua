ce4bfd6eacdd30934e8a6a05e18d9a527d79eeb3
lumetas <lumetas506@gmail.com>
2026-02-18 18:55:56 +0500

    claan plugins

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -3,33 +3,33 @@ return function(lnpm)
 	lnpm.load('nvim-tree/nvim-web-devicons', nil, {lrule = function(next) vim.defer_fn(next, 1000) end})
 	lnpm.load('kevinhwang91/promise-async')
 
-	lnpm.load('kylechui/nvim-surround', function(surround) surround.setup() end, {lrule = function (next) vim.defer_fn(next, 1000) end})
+	-- lnpm.load('kylechui/nvim-surround', function(surround) surround.setup() end, {lrule = function (next) vim.defer_fn(next, 1000) end})
 
-	lnpm.load('nvim-treesitter/nvim-treesitter', function(ts)
-		ts.setup({
-			ensure_installed = {
-				'php', 'javascript', 'css', 'html', 'markdown', 
-				'lua', 'go', 'python', 'blade'
-			},
-			-- Опционально: автоматическая установка недостающих парсеров
-			auto_install = true,
+-- 	lnpm.load('nvim-treesitter/nvim-treesitter', function(ts)
+-- 		ts.setup({
+-- 			ensure_installed = {
+-- 				'php', 'javascript', 'css', 'html', 'markdown', 
+-- 				'lua', 'go', 'python', 'blade'
+-- 			},
+-- 			-- Опционально: автоматическая установка недостающих парсеров
+-- 			auto_install = true,
+--
+-- 			-- Остальные настройки (пример):
+-- 			highlight = {
+-- 				enable = true,
+-- 				additional_vim_regex_highlighting = false,
+-- 			},
+-- 			indent = {
+-- 				enable = true,
+-- 			},
+-- 		})
+-- 	end, {
+-- 	lrule = function(next) 
+-- 		vim.defer_fn(next, 100) 
+-- 	end
+-- })
 
-			-- Остальные настройки (пример):
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end, {
-	lrule = function(next) 
-		vim.defer_fn(next, 100) 
-	end
-})
-
-lnpm.load('mattn/emmet-vim', nil, {lrule = function(next) vim.defer_fn(next, 1000) end})
+-- lnpm.load('mattn/emmet-vim', nil, {lrule = function(next) vim.defer_fn(next, 1000) end})
 
 -- lnpm.load('AndrewRadev/quickpeek.vim', nil)
 
