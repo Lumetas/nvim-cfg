dee347cb39e82b8b6f03472e8e85e94e4a3ffce8
lumetas <lumetas506@gmail.com>
2026-02-20 21:54:12 +0500

    images

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -41,4 +41,45 @@ vim.keymap.set('n', 'zx', function()
 	vim.cmd('Yazi')
 end, { noremap = true })
 	end})
+
+
+
+	lnpm.load("Skardyy/neo-img", function(img) 
+	img.setup({
+	  supported_extensions = {
+		png = true,
+		jpg = true,
+		jpeg = true,
+		tiff = true,
+		tif = true,
+		svg = true,
+		webp = true,
+		bmp = true,
+		gif = true, -- static only
+		docx = true,
+		xlsx = true,
+		pdf = true,
+		pptx = true,
+		odg = true,
+		odp = true,
+		ods = true,
+		odt = true
+	  },
+
+	  ----- Important ones -----
+	  size = "60%",  -- size of the image in percent
+	  center = true, -- rather or not to center the image in the window
+	  ----- Important ones -----
+
+	  ----- Less Important -----
+	  auto_open = true,   -- Automatically open images when buffer is loaded
+	  oil_preview = false, -- changes oil preview of images too
+	  backend = "auto",   -- auto / kitty / iterm / sixel
+	  resizeMode = "Fit", -- Fit / Stretch / Crop
+	  offset = "1x1",     -- that exmp is 2 cells offset x and 3 y.
+	  ttyimg = "local"    -- local / global
+	  ----- Less Important -----
+	})
+	end, {lrule = function(next) vim.defer_fn(next, 1000) end})
+
 end
